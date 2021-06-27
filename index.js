1cdc2ff9ce30cade564e53ed077c2dbab165293d
peterczg@163.com <peterczg@163.com>
2021-06-27 19:12:56 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+export { calcFrames } from "./src/fps-calculation";
---
+++ src/fps-calculation.js
@@ -0,0 +1,31 @@
+/**
+ * calculate the fps or function call per sec.
+ * @param {function} cb
+ * @param {object} context
+ * @param {boolean} enableConsole log the current FPS
+ */
+export function calcFrames(cb, context, enableConsole = false) {
+  let prevFrames = 0;
+  let frames = 0;
+  let start = performance.now();
+  return function () {
+    cb.call(context || this);
+    if (performance.now() - start <= 1000) {
+      frames++;
+    } else {
+      enableConsole && console.log("current fps：" + frames);
+      // tag
+      cb.__frames__ = frames;
+      // change hook
+      prevFrames &&
+        frames !== prevFrames &&
+        cb.__onFPSChange__ &&
+        cb.__onFPSChange__(frames, prevFrames);
+      // refresh hook
+      cb.__onRefresh__ && cb.__onRefresh__();
+      // reset
+      frames = 0;
+      start = performance.now();
+    }
+  };
+}
